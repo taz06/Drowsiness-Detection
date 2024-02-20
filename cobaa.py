c826c5268acd42d88c3b07d9766a9b53899c9270
athirarps <athiramadhanova17@gmail.com>
2024-02-20 16:23:59 +0700

    donlod dataset

--- cobaa.py
+++ cobaa.py
@@ -1,17 +1,20 @@
+import tarfile
 import dlib
 import cv2
 import imutils
 import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import wget
 
 
 #DOWNLOAD DATASET
-os.chdir("custom-dlib-landmark")
+os.chdir("dataset")
 print("Current directory:", os.getcwd())
 
-wget http://dlib.net/files/data/ibug_300W_large_face_landmark_dataset.tar.gz
+#wget.download ('http://dlib.net/files/data/ibug_300W_large_face_landmark_dataset.tar.gz') 
+
 #compress
-#tar-xvf ibug_300W_large_face_landmark_dataset.tar.gz
+tarfile.open('ibug_300W_large_face_landmark_dataset.tar.gz')
 
 
